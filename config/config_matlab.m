531e818a0aae1d9ca3c37c9954e5abb04513df54
pawelkudela <pk@imp.gda.pl>
2018-12-02 13:21:38 +0100

    add also subfolders to the matlab path

--- config/config_matlab.m
+++ config/config_matlab.m
@@ -8,10 +8,10 @@ config_path=fullfile( pathstr );
 projectroot=fullfile(pathstr(1:end-6));
 % create src_path based on standard folder structure
 src_path = fullfile( projectroot, 'src', filesep);
-% add src_path to the matlab path
-addpath(src_path);
+% add src_path to the matlab path with subfolders
+addpath(genpath(src_path));
 disp(['projectroot path: ',projectroot]);
-disp(['added src_path: ', src_path]);
+disp(['added src_path with subfolders: ', src_path]);
 filename = fullfile(src_path,'project_paths.mat');
 % save path definitions to mat file at src_path for future use
 save(filename,'projectroot','src_path');
