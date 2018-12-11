7fd3e5419e562b092b5f318bf1c69b6faae1f854
pawelkudela <pk@imp.gda.pl>
2018-12-11 09:54:39 +0100

    adding laser2dispersion_matrix_script, improving fun2log_matrix_script

---
+++ src/data_processing/laser2dispersion_matrix_script.m
@@ -0,0 +1,88 @@
+% process raw data test script template
+
+clc;close all;clear all;
+
+% load projectroot path
+load project_paths projectroot src_path;
+
+% create path to the experimental raw data folder
+raw_data_path=fullfile( projectroot, 'data','raw','exp', filesep );
+
+% filename of data to be processed
+filename = '643p_pulse_5ms_x100_7Vpp_up250kHz.mat';
+
+% load raw experimental data file
+load([raw_data_path,filename]);
+
+%% START DATA PROCESSING
+%
+Lenght = 0.9*sqrt(2);  
+
+        % Unit vecotrs      
+Fs =  1/(time(3)-time(2));    % sampling frequency
+L = length(Data(1,:));
+NFFT = 2^nextpow2(L); 
+%f_vec = Fs/2*linspace(0,1,NFFT/2+1);  % frequency vector
+
+[points frames] = size(Data);
+deltay = Lenght/(points-1);                   
+ky1 = (0:points-1)/points*2*pi/deltay;             % wavenumebr 1/m
+%ky1 = (0:points-1)/points/deltay;             % angular wavenumebr rad/m
+ky_vec = ky1-1/2*ky1(end);
+
+% same as above
+%deltaY = Lenght/(points-1);
+%kY1 = mod( 1/2 + (0:(points-1))/points , 1 ) - 1/2;
+%ky_vec2 = kY1*(2*pi/deltaY);
+
+
+L_padded = 8*1024;
+freqwave = fftshift(fftn(Data,[L_padded L_padded]));
+f_vec = Fs/2*linspace(0,1,L_padded/2);  % frequency vector
+%f_vec = Fs/2*linspace(0,1,L_padded/2)*2*pi;  % angular frequency vector
+figure
+set(gcf,'Color','w','Position',[100 100 640 480])
+%imagesc(f_vec/10^3, ky_vec/1000, (abs(freqwave(:,floor(L_padded/2):L_padded))));   set(gca,'YDir','normal');  
+C=(abs(freqwave(:,floor(L_padded/2):L_padded)));
+
+%imagesc(f_vec, ky_vec, C);   set(gca,'YDir','normal');  %rad/m
+[m,n]=size(C);
+fmin=min(f_vec);
+fmax=max(f_vec);
+fvec2=fmin:(fmax-fmin)/(n-1):fmax;
+kmin=min(ky_vec);
+kmax=max(ky_vec);
+kvec2=kmin:(kmax-kmin)/(m-1):kmax;
+imagesc(fvec2, kvec2, C);   
+set(gca,'YDir','normal');  
+set(gca,'Fontsize',16)
+%xlabel('Angular frequency (rad/s)')
+xlabel('Frequency (Hz)')
+ylabel('k_y (1/m)')
+%colormap(cmap);
+axis tight; 
+b = max(caxis);  c = min(caxis);  
+caxis([c b/5]); 
+%interim_data_exp = 2*raw_data_exp;
+return;
+%% END DATA PROCESSING
+
+% create path to the experimental interim data folder
+interim_data_path=fullfile( projectroot, 'data','interim','exp', filesep );
+
+% filename of processed data
+interim_filename = 'interim_data_testfile.txt';
+
+% save processed data to interim (intermidiate) data folder
+save([interim_data_path,interim_filename],'interim_data_exp','-ascii');
+
+% add marker (empty file '.exist' ) indicating that 
+% the processing results finished successfully and exist
+
+output_filename_marker = [interim_data_path,'.exist'];
+
+if ~exist(output_filename_marker, 'file' )
+    fid = fopen(output_filename_marker,'w');
+    fclose(fid);
+end
+% comment
--- src/tools/fun2log_matrix_script.m
+++ src/tools/fun2log_matrix_script.m
@@ -9,31 +9,83 @@ load project_paths projectroot src_path;
 % create path to the numerical raw data folder
 raw_data_path=fullfile( projectroot, 'data','raw','num',[model,'-out'], filesep );
 
-% filename of data to be processed
-filename = 'dispersion3D-3nodes-A0-4-2MHz.mat';
+% create path to the experimental interim data folder
+interim_data_path=fullfile( projectroot, 'data','interim','exp', filesep );
 
+% filename of data to be processed
+%filename = 'dispersion3D-3nodes-A0-4-2MHz.mat';
+%filename = 'dispersion3D-3nodes-A0-4-2MHz-v3.mat';
+filename = 'dispersion3D-3nodes-A0-4-2MHz-v4.mat';
+
+% filename of interim experimental data
+filename_exp = 'Krzywa.mat';
 % load raw numerical data file
 raw_data_num = load([raw_data_path,filename]);
+load([interim_data_path,filename_exp]);
 %% START DATA PROCESSING
+%%
+figure('Name','Wavenumber - Frequency')
+%set(gcf,'Color','w','Position',[1921 100 1280 900]);
+set(gcf,'Color','w');
+imagesc(f_vec/1e3, k_vec_interp, abs(Wavenumber_Frequency));   set(gca,'YDir','normal'); 
 
-omega=raw_data_num.om(1:80:end);
-wavenumber=raw_data_num.wavenumber(1:80:end);
-plot(omega,wavenumber);
-m=length(omega);
-H=logical(sparse(m,m));
-kw = max(wavenumber)*linspace(0,1,m); % liearly equally spaced vector of frequencies
-%om = interp1(wavenumber,omega,kw','linear','extrap');
-tic
-for i=1:m
-    [~,I] = min(abs(wavenumber(i)-kw));
+set(gca,'Fontsize',14)
+xlabel('f [kHz]')
+ylabel('k [1/m]')
+colormap(jet(255)); axis tight; 
+caxis([0 max(caxis)/50]); 
+%%
+f_vec_max= f_vec(end);
+k_vec_interp_max = k_vec_interp(end);
+[km,fn] = size(Wavenumber_Frequency);
+fmin=f_vec(1);
+fmax=f_vec(end);
+fvec2=fmin:(fmax-fmin)/(fn-1):fmax;
+% numerical dispersion curve
+freq=raw_data_num.om/(2*pi);
+wavenumber=raw_data_num.wavenumber;
+% interpolation would be unnecessary if dispersion curves will be evaluated
+% at the same frequencies as in the experiment
+kvec2 = interp1(freq, wavenumber,fvec2,'linear','extrap');
+
+hold on;
+%plot(freq(1:25000)/1e3,wavenumber(1:25000),'y');
+plot(fvec2/1e3,kvec2,'y');
+
+
+% convert numerical dispersion curve into logical matrix
+%H=logical(sparse(km,fn));
+H=logical(zeros(km,fn));
+for i=1:fn
+    [~,I] = min(abs( kvec2(i) - k_vec_interp ));
     H(I,i) = 1;
 end
-toc
-H = flipud(H); % only for checking figure; comment later on for algorithm
-figure;
-spy(H)
 
+% apply logical matrix to experimental dispersion curve matrix
+dispersion = H.*abs(Wavenumber_Frequency)/max(max(abs(Wavenumber_Frequency)));
+% H = flipud(H); % only for checking figure; comment later on for algorithm
+% figure;
+% spy(H)
+figure('Name','Wavenumber - Frequency')
+%set(gcf,'Color','w','Position',[1921 100 1280 900]);
+set(gcf,'Color','w');
+imagesc([0 f_vec_max/1e3], [0 k_vec_interp_max], dispersion);   set(gca,'YDir','normal'); 
+
+set(gca,'Fontsize',14)
+xlabel('f [kHz]')
+ylabel('k [1/m]')
+colormap(jet(255)); axis tight; 
+caxis([0 max(caxis)/50]); 
+
+%
+[i,j] = find(H==true);
+obj=0;
+for k =1:fn
+obj = obj + (sqrt(dispersion(i(k),j(k)).^2))/fn;
+end
+obj
 return;
+
 %% END DATA PROCESSING
 
 % create path to the experimental interim data folder
