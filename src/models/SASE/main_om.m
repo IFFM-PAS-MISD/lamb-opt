1d1ecdbc3a98e4e6302b6a5724b7604dbb78afcd
pawelkudela <pk@imp.gda.pl>
2018-12-13 09:57:32 +0100

    developed mode-tracing algorithm in main_om

--- src/models/SASE/main_om.m
+++ src/models/SASE/main_om.m
@@ -81,7 +81,7 @@ end
 
 
 %%
-c='rgbkrbkrbgkrbgkrbgkrbgkrbgkrbgkrbgkrbgkrbgkrgbkrbgkrbgkrgbkrbkrbgkrbgkrbgkrbgkrbgkrbgkrbgkrbgkrbgkrgbkrbgkrbgkrgbkrbkrbgkrbgkrbgkrbgkrbgkrbgkrbgkrbgkrbgkrgbkrbgkrbgk';
+c='bgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmyk';
 figure(12); hold on;
 for k=1:num_of_modes
     s=[c(k),'.'];
@@ -137,83 +137,85 @@ axis([0 500000/1e3 0 10000]);
 % Pade expansion method
 
 om_new = om_real;
-ind = zeros(length(M),number_of_wavenumber_points);
+%ind = zeros(length(M),number_of_wavenumber_points);
 cg_new=cg;
 for i=1:num_of_modes
     ind(i,number_of_wavenumber_points)=i;
 end
 for i=1:num_of_modes
     I=i;cc=0;
-for k=number_of_wavenumber_points-1:-1:4
+for k=number_of_wavenumber_points:-1:2
    
     if (cc==0) % first order Taylor approximation
         cc=cc+1;
-        gamma1=cg(i,k-1);
-        gamma0=om_real(i,k-1);  
-        om_t=gamma0-gamma1*k_step; % first order Taylor approximation (backward)
-        cg_t=2*cg2-cg1; % linear Taylor approximation
-    elseif (cc==1) % second order Taylor approximation
-        
-    end
-    if(I~=i && cc==0)
-        
-        cc=cc+1;
-        cg0=cg(i,k);
-        cg1=cg(i,k-1);
-        cg2=cg(I,k-2);
-    
-        gamma0=om_real(I,k-2);  
-    elseif (I~=i && cc==1)
-        cc=cc+1;
-        cg0=cg(i,k);
-        cg1=cg(I,k-1);
-        cg2=cg(I,k-2);
-    
-        gamma0=om_real(I,k-2);  
-    elseif (I~=i && cc>=2)
-        cc=cc+1;
         cg0=cg(I,k);
         cg1=cg(I,k-1);
-        cg2=cg(I,k-2);
-    
-        gamma0=om_real(I,k-2);
+        gamma0=om_real(I,k); 
+        gamma1=cg0;
+        om_t=gamma0-gamma1*k_step; % first order Taylor approximation (backward)
+        %cg_t=cg1; 
+        cg_t=cg0; 
+        delta_om = abs((om_t-om_real(:,k-1))/om_t); % deviation of angular frequency in Taylor method
+        delta_cg = abs((cg_t-cg(:,k-1))/cg_t);  % deviation of group velocity in Taylor method
+        delta = delta_om.^2+delta_cg.^2;
+        [a,I] = min(delta_om);
+        %ind(i,k-1) = I;
+        om_new(i,k-1) = om_real(I,k-1);
+        cg_new(i,k-1) = cg(I,k-1);
+    elseif (cc==1) % second order Taylor approximation
+        cc=cc+1; 
+        cg0=cg(I,k+1);
+        cg1=cg(I,k);
+        gamma0=om_real(I,k); 
+        gamma1=cg0;
+        gamma2=1/2*(cg1-cg0)/k_step;
+        om_t=gamma0-gamma1*k_step-gamma2*k_step^2; % second order Taylor approximation (backward)
+        cg_t=2*cg1-cg0; % linear Taylor approximation
+        delta_om = abs((om_t-om_real(:,k-1))/om_t); % deviation of angular frequency in Taylor method
+        delta_cg = abs((cg_t-cg(:,k-1))/cg_t);  % deviation of group velocity in Taylor method
+        delta = delta_om.^2+delta_cg.^2;
+        [a,I] = min(delta_om);
+        %ind(i,k-1) = I;
+        om_new(i,k-1) = om_real(I,k-1);
+        cg_new(i,k-1) = cg(I,k-1);
     else
-        cg0=cg(i,k);
-        cg1=cg(i,k-1);
-        cg2=cg(i,k-2);
+        cc=cc+1; 
+        cg0=cg(I,k+2);
+        cg1=cg(I,k+1);
+        cg2=cg(I,k);
     
-        gamma0=om_real(i,k-2);
+        gamma0=om_real(I,k);
+        gamma1=cg2;
+        gamma2=1/2*(cg2-cg1)/k_step;
+        gamma3=1/6*(cg2-2*cg1+cg0)/(k_step^2);
+    
+        beta2=(gamma2^2-gamma3*gamma1)/(gamma1^2-gamma2*gamma0);
+        beta1=-(gamma2+gamma0*beta2)/gamma1;
+        alpha0=gamma0;
+        alpha1=gamma1+gamma0*beta1;
+    
+        %om_p=(alpha0 + alpha1 * k_step)/(1 + beta1*k_step + beta2*k_step^2); %Pade expansion of order [1/2](forward)
+        %om_p=(alpha0 - alpha1 * k_step)/(1 -( beta1*k_step + beta2*k_step^2)); % Pade expansion of order [1/2](backward)
+        om_t=gamma0-gamma1*k_step-gamma2*k_step^2-gamma3*k_step^3; % Third order Taylor approximation (backward)
+    
+        cg_t=2*cg2-cg1; % linear Taylor approximation
+        
+        delta_om = abs((om_t-om_real(:,k-1))/om_t); % deviation of angular frequency in Taylor method
+        delta_cg = abs((cg_t-cg(:,k-1))/cg_t);  % deviation of group velocity in Taylor method
+        delta = delta_om.^2+delta_cg.^2;
+        %[a,I] = min(delta);
+        [a,I] = min(delta_om);
+        
+        %ind(i,k-1) = I;
+        om_new(i,k-1) = om_real(I,k-1);
+        cg_new(i,k-1) = cg(I,k-1);
+        if(I~=i)
+            cc=0;
+            %disp('change curve');
+        end
     end
-    
+
    
-    gamma1=cg2;
-    gamma2=1/2*(cg2-cg1)/k_step;
-    gamma3=1/6*(cg2-2*cg1+cg0)/(k_step^2);
-    
-    beta2=(gamma2^2-gamma3*gamma1)/(gamma1^2-gamma2*gamma0);
-    beta1=-(gamma2+gamma0*beta2)/gamma1;
-    alpha0=gamma0;
-    alpha1=gamma1+gamma0*beta1;
-    
-    %om_p=(alpha0 + alpha1 * k_step)/(1 + beta1*k_step + beta2*k_step^2); %Pade expansion of order [1/2](forward)
-    om_p=(alpha0 - alpha1 * k_step)/(1 -( beta1*k_step + beta2*k_step^2)); % Pade expansion of order [1/2](backward)
-    om_t=gamma0-gamma1*k_step-gamma2*k_step^2-gamma3*k_step^3; % Third order Taylor approximation (backward)
-    
-    cg_t=2*cg2-cg1; % linear Taylor approximation
-    
-    delta_om = abs((om_p-om_real(:,k-3))/om_p); % deviation of angular frequency in Pade method
-    delta_cg = abs((cg_t-cg(:,k-3))/cg_t);  % deviation of group velocity 
-    delta = delta_om.^2+delta_cg.^2;
-    
-%     delta_om = abs((om_t-om_real(:,k-3))/om_t); % deviation of angular frequency in Taylor method
-%     delta_cg = abs((cg_t-cg(:,k-3))/cg_t);  % deviation of group velocity in Taylor method
-%     delta = delta_om.^2+delta_cg.^2;
-%     
-    
-    [a,I] = min(delta_om);
-    om_new(i,k-3) = om_real(I,k-3);
-    cg_new(i,k-3) = cg(I,k-3);
-    ind(i,k-3) = I;
     
     
 %     close all;
@@ -230,18 +232,41 @@ for k=number_of_wavenumber_points-1:-1:4
 end
 end
 %%
+
 figure(16); hold on;
 for k=1:num_of_modes
     s=[c(k),'.'];
     plot(om_new(k,:)/2/pi/1e3,om_new(k,:)./wavenumber,s,'LineWidth',2);
 end
 Hxl=xlabel('Frequency [kHz]');
+Hyl=ylabel('Phase velocity');
 set(Hxl,'FontSize',12);set(Hyl,'FontSize',12);
 set(gcf,'Color','white');set(gca,'FontSize',11);
 title([num2str(beta(1)),' [deg]']);
 axis([0 5000000/1e3 0 10000]);
 
+figure(17); hold on;
+for k=1:num_of_modes
+    s=[c(k),'.'];
+    plot(om_new(k,:)/2/pi/1e3,cg_new(k,:),s,'LineWidth',2);
+end
+Hxl=xlabel('Frequency [kHz]');
+Hyl=ylabel('Group velocity');
+set(Hxl,'FontSize',12);set(Hyl,'FontSize',12);
+set(gcf,'Color','white');set(gca,'FontSize',11);
+title([num2str(beta(1)),' [deg]']);
+axis([0 5000000/1e3 0 10000]);
 
+figure(18); hold on;
+
+plot(om_new(:,:)/2/pi/1e3,wavenumber,'LineWidth',2);
+
+Hxl=xlabel('Frequency [kHz]');
+Hyl=ylabel('Wavenumber [1/m]');
+set(Hxl,'FontSize',12);set(Hyl,'FontSize',12);
+set(gcf,'Color','white');set(gca,'FontSize',11);
+title([num2str(beta(1)),' [deg]']);
+axis([0 5000000/1e3 0 1800]);
 return;
 %% Roots sorting
 switch sorting
