aa1ba9a9e671d7ceae2ed62b99322affe9081d3e
unknown <Acoustic Lens 1@PCAL1.imp.gda.pl>
2019-02-27 12:23:38 +0100

    Added

--- src/models/common/fabric_composites/compfabricprop.m
+++ src/models/common/fabric_composites/compfabricprop.m
@@ -2,9 +2,6 @@ function [Q11,Q12,Q13,Q21,Q22,Q23,Q31,Q32,Q33,Q44,Q55,Q66,rho] = ...
     compfabricprop(fiberType, h_p, h_f, h_w, a_f, a_w, g_f, g_w, vol_0, ...
     e11_m, ni12_m, rho_m, e11_f, e22_f, ni12_f, ni23_f, rho_f)
 % COMPFABRICPROP   Calculation of the effective mechanical properties of fabric composite 
-%    optional: more details about the function than in the H1 line 
-%    optional: more details about the function than in the H1 line 
-%    optional: more details about the function than in the H1 line 
 % 
 % Syntax: [output1,output2] = compfabricprop(input1,input2,input3) 
 % 
