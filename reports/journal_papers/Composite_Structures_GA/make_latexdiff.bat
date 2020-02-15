70422eae71afc2da9f05ae47c0ca6a971a2eed67
pawelkudela <pk@imp.gda.pl>
2020-02-15 14:41:38 +0100

    latexdiff bat, spie template

---
+++ reports/journal_papers/Composite_Structures_GA/make_latexdiff.bat
@@ -0,0 +1,10 @@
+
+SETLOCAL
+set latexdiffpath="C:\Strawberry\perl\bin\latexdiff\latexdiff.pl"
+set original=Identification_GA.tex
+set revised=Identification_GA_R1.tex
+set output=diff.tex
+
+%latexdiffpath% %original% %revised%>%output%
+
+ENDLOCAL
